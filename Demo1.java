12a6fdcd91856eaf3a679f63934bb81f0fbc7588
Rashmi4555 <rashmibm3579@gmail.com>
2025-04-24 12:57:56 +0530

    Save java

---
+++ Demo1.java
@@ -0,0 +1,17 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        int num1 = 10; 
+        int num2 = 5;  
+       
+        int add = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1 * num2;
+        int div = num1/num2;
+        
+
+        System.out.println("Addition Result: " +add);
+        System.out.println("Subtraction Result: " + sub);
+        System.out.println("Multiplication Result: " + mul);
+        System.out.println("Division Result: " + div);
+    }
+}
