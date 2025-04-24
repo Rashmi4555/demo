a3351868dfca7f52dd8717421d01c41a6538ade1
Rashmi4555 <rashmibm3579@gmail.com>
2025-04-24 11:26:35 +0530

    Save

--- demo.java
+++ demo.java
@@ -1,12 +1,13 @@
 public class demo {
     public static void main(String[] args) {
-        int num1 = 10; // First number
-        int num2 = 5;  // Second number
+        int num1 = 10; 
+        int num2 = 5;  
        
-        int add = num1 + num2;
-        int sub = num1 - num2;
+        int mul = num1 * num2;
+        int div = num1 / num2;
+        
 
-        System.out.println("Addition Result: " +add);
-        System.out.println("Subtraction Result: " + sub);
+        System.out.println("Multiplication Result: " +mul);
+        System.out.println("Division Result: " + div);
     }
 }
